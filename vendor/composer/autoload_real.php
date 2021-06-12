0396368bd5850fef382dc02c0cbef27fccd9fc04
Ahmadreza Nozaeem <aminz.uiux@gmail.com>
2021-06-13 00:51:51 +0430

    dumpautoload and change paths

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,10 @@ class ComposerAutoloaderInitad19b6a431b789933d58591205f8f073
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitad19b6a431b789933d58591205f8f073', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInitad19b6a431b789933d58591205f8f073', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
