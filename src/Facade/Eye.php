db36b1685a1e1670720731c2a100dd4b8bd63628
Ahmadreza Nozaeem <83532676+ami-hp@users.noreply.github.com>
2021-06-20 17:43:35 +0430

    Update Eye.php

--- src/Facade/Eye.php
+++ src/Facade/Eye.php
@@ -11,9 +11,9 @@ use Illuminate\Support\Facades\Facade;
  * @package App\Services
  * @method static record()
  * @method static prepare_for_database()
- * @method static setAndGetViews(string $cache_name , int $id = 0)
- * @method static readyTotalChart(string|array $type = "total" , Boolean $json = true)
- * @method static readyDetailsChart(string|array $type = "total" , int|string $page_id = 0 , Boolean $json = true)
+ * @method static setAndGetViews(string $cache_name ,  int $id = 0)
+ * @method static readyTotalChart(string|array $type = "total" , $timeType = "gregorian",  Boolean $json = true)
+ * @method static readyDetailsChart(string|array $type = "total" , int|string $page_id = 0 , $timeType = "gregorian", Boolean $json = true)
  */
 class Eye extends Facade
 {
